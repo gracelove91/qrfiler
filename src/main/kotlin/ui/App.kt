bb07a269954d3bd999bbb1de3c7c8668a3779760
gracelove91 <govlmo91@gmail.com>
2026-05-22 07:25:25 +0900

    fix: replace remaining Compose Color.value.toInt() with SkiaColor
    - QrCodeImage.kt: use SkiaColor.BLACK / WHITE instead of Color.Black.value.toInt()
    
    - App.kt: remove unused Color import

--- src/main/kotlin/ui/App.kt
+++ src/main/kotlin/ui/App.kt
@@ -18,7 +18,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.awt.ComposeWindow
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.sun.net.httpserver.HttpServer
 import java.awt.datatransfer.DataFlavor
